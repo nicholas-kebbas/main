a9c96057d7db6cc42ec7052a27f69bb6538bdf55
Nick Kebbas <nick@nick-VirtualBox.(none)>
2014-08-26 00:39:41 -0700

    second commit

--- profile/config/routes.rb
+++ profile/config/routes.rb
@@ -1,6 +1,6 @@
 Profile::Application.routes.draw do
   resources :mains
-
+  get '/main/', to: 'mains#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

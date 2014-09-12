3d201f6c70eaac84d283390885fb34bbcad2c1cc
Nick Kebbas <nick@nick-VirtualBox.(none)>
2014-09-12 12:12:05 -0700

    set assets compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,7 @@ Profile::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.paths << "#{Rails.root}/public/assets/minimal_classic_skin"
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true

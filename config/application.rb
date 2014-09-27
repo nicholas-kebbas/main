68fa47260503f3064a4753fb59d3511c84e0060f
Nick Kebbas <nick@nick-VirtualBox.(none)>
2014-09-27 00:24:49 -0700

    profile

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Profile
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.serve_static_assets = true
+    config.assets.precompile += %w( landing.css )
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
